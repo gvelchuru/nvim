17f5bd1d48b70787c16b61a142f4c96b4af59328
Gautham Velchuru <gautham.velchuru@coinbase.com>
2024-07-10 15:53:44 -0700

    merging

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = {"markdown", "markdown_inline", "java", "python", "sql", "javascript", "yaml", "rust", "vim", "ruby", "lua" },
+  --ensure_installed = {"diff", "git_rebase", "markdown", "markdown_inline", "java", "python", "sql", "javascript", "yaml", "rust", "vim", "ruby", "lua" },
+  ensure_installed = {"all"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
