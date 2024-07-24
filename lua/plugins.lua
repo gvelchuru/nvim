a4f361e91073acfb37cbd8ef49b1decbdd1c9cff
Gautham Velchuru <gautham.velchuru@coinbase.com>
2024-07-24 13:41:30 -0700

    merging

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -159,6 +159,14 @@ return require("lazy").setup({
     'kevinhwang91/nvim-fundo', dependencies = 'kevinhwang91/promise-async',
      config = function() require('fundo').install() end
 	},
+	{
+  "oysandvik94/curl.nvim",
+  cmd = { "CurlOpen" },
+  dependencies = {
+  "nvim-lua/plenary.nvim",
+},
+config = true,
+},
 
 	--C
 	{ "chrisbra/csv.vim" },
