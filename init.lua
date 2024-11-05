ea3ec20e159e446ac64297f0b948d4806b288dd1
Gautham Velchuru <gautham.velchuru@coinbase.com>
2024-11-05 10:58:00 -0600

    noice

--- init.lua
+++ init.lua
@@ -65,6 +65,7 @@ require('telescope').setup {
   },
 }
 require('telescope').load_extension('fzy_native')
+require("telescope").load_extension("noice")
 --require("barbecue.ui").toggle(true)
 require('Comment').setup()
 require('leap').create_default_mappings()
